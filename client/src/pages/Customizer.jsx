02756e2fc05e6910ea84f366124dc4735fc024d8
Lê  Hoàng Vũ <68913@thegioididong.com>
2023-04-20 13:44:40 +0700

    change server deploy

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -91,7 +91,7 @@ const Customizer = () => {
             // call our backend to generate an ai image!
             setGeneratingImg(true);
             console.log('prompt', prompt)
-            const response = await fetch('http://localhost:8080/api/v1/dalle', {
+            const response = await fetch('https://threed-ai-4tmi.onrender.com/api/v1/dalle', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
