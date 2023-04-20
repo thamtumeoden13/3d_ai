0face3bcb4f86a997338764f778e2d4a7a1dfe8e
Lê  Hoàng Vũ <68913@thegioididong.com>
2023-04-20 14:17:03 +0700

    change url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
   development: {
-    backendUrl: "https://threed-ai-4tmi.onrender.com/api/v1/dalle",
+    backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://threed-ai-4tmi.onrender.com/api/v1/dalle",
   },
 };
 
